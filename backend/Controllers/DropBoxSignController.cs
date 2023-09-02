75731b35fc183587b51d074f7749a1bca23e851f
gtktorres <gtktorres@gmail.com>
2023-09-01 22:52:43 -0600

    initial commit

---
+++ backend/Controllers/DropBoxSignController.cs
@@ -0,0 +1,30 @@
+﻿using Dropbox.Sign.Model;
+using Microsoft.AspNetCore.Mvc;
+using DBS = DropBoxSign;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DropBoxSignController : ControllerBase
+    {
+        [HttpGet("GetAccount")]
+        public async Task<AccountGetResponse> GetAccount()
+        {
+            return await new DBS().AccountGet();
+        }
+
+        [HttpPost("CreateEmbeddedSignature")]
+        public async Task<SignatureRequestGetResponse> CreateSignature(SignatureRequestCreateEmbeddedRequest body)
+        {
+            return await new DBS().CreateSignature(body);
+        }
+
+        [HttpGet("GetEmbeddedSignature")]
+        public async Task<EmbeddedSignUrlResponse> GetEmbeddedSignURL(string signature_id)
+        {
+            return await new DBS().GetEmbeddedSignURL(signature_id);           
+        }
+
+    }
+}

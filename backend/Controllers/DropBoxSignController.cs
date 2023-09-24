70675b2644b3294977fe7654986929ee3d6b2341
gtktorres <gtktorres@gmail.com>
2023-09-23 21:13:48 -0600

    requester should be able create a drafted contract with an embedded template via api call, update a file(s), remove or add users to template, create a contract(s) with submitted documents or a given template, get the signature url of a contract, download files or download files as fileurl or download files as datauri and delete contract template

--- backend/Controllers/DropBoxSignController.cs
+++ backend/Controllers/DropBoxSignController.cs
@@ -1,5 +1,6 @@
 ﻿using Dropbox.Sign.Model;
 using Microsoft.AspNetCore.Mvc;
+using static DropBoxSign;
 using DBS = DropBoxSign;
 
 namespace backend.Controllers
@@ -7,23 +8,108 @@ namespace backend.Controllers
     [Route("api/[controller]")]
     public class DropBoxSignController : ControllerBase
     {
+        //Authentication
+
+        [HttpPost("GenerateOAuthToken")]
+        public async Task<OAuthTokenResponse> GenerateOAuthToken([FromBody] OAuthTokenGenerateRequest body)
+        {
+            return await new DBS().OauthTokenGenerate(body);
+        }
+
+        [HttpPost("RefreshOAuthToken")]
+        public async Task<OAuthTokenResponse> RefreshOAuthToken([FromBody] OAuthTokenRefreshRequest body)
+        {
+            return await new DBS().OauthTokenRefresh(body);
+        }
+
+        //Accounts
+
+        [HttpPost("CreateAccount")]
+        public async Task<AccountCreateResponse> AccountCreate([FromBody] AccountCreateRequest body)
+        {
+            return await new DBS().AccountCreate(body);
+        }
+
         [HttpGet("GetAccount")]
-        public async Task<AccountGetResponse> GetAccount()
+        public async Task<AccountGetResponse> AccountGet()
         {
             return await new DBS().AccountGet();
         }
 
-        [HttpPost("CreateEmbeddedSignature")]
-        public async Task<SignatureRequestGetResponse> CreateSignature([FromBody]SignatureRequestCreateEmbeddedRequest body)
+        [HttpGet("UpdateAccount")]
+        public async Task<AccountGetResponse> AccountUpdate(AccountUpdateRequest body)
         {
-            return await new DBS().CreateSignature(body);
+            return await new DBS().AccountUpdate(body);
         }
 
-        [HttpGet("GetEmbeddedSignature")]
-        public async Task<EmbeddedSignUrlResponse> GetEmbeddedSignURL(string signature_id)
+        [HttpGet("DeleteAccount")]
+        public async Task<string> ApiAppDelete(string clientId)
         {
-            return await new DBS().GetEmbeddedSignURL(signature_id);           
+            return await new DBS().ApiAppDelete(clientId);
         }
+
+        //Contracts
+        //create a drafted contract with an embedded template
+        [HttpPost("CreateContractTemplate")]
+        public async Task<TemplateCreateEmbeddedDraftResponse> TemplateCreateEmbeddedDraft([FromBody]TemplateCreateEmbeddedDraftRequest body)
+        {
+            return await new DBS().TemplateCreateEmbeddedDraft(body);
+        }
+        //update a files
+        [HttpPost("TemplateUpdateFiles")]
+        public async Task<TemplateUpdateFilesResponse> TemplateUpdateFiles(string template_id, [FromBody]TemplateUpdateFilesRequest request)
+        {
+            return await new DBS().TemplateUpdateFiles(template_id, request);
+        }
+        //add users to template
+        [HttpPost("AddUserToTemplate")]
+        public async Task<TemplateGetResponse> TemplateAddUser(string template_id, [FromBody]TemplateAddUserRequest request)
+        {
+            return await new DBS().TemplateAddUser(template_id, request);
+        }
+        //remove users to template
+        [HttpPost("RemoveUserFromTemplate")]
+        public async Task<TemplateGetResponse> TemplateRemoveUser(string template_id, [FromBody] TemplateRemoveUserRequest request)
+        {
+            return await new DBS().TemplateRemoveUser(template_id, request);
+        }
+        //create a contracts with submitted documents
+        [HttpPost("CreateEmbeddedSignature")]
+        public async Task<SignatureRequestGetResponse> SignatureRequestCreateEmbedded([FromBody] SignatureRequestCreateEmbeddedRequest body)
+        {
+            return await new DBS().SignatureRequestCreateEmbedded(body);
+        }
+        //create a contracts with a given template
+        [HttpPost("CreateEmbeddedSignatrueWithTemplate")]
+        public async Task<SignatureRequestGetResponse> SignatureRequestCreateEmbeddedWithTemplate(SignatureRequestCreateEmbeddedWithTemplateRequest body)
+        {
+            return await new DBS().SignatureRequestCreateEmbeddedWithTemplate(body);
+        }
+        //create a contracts
+        [HttpPost("BulkSendEmbeddedTemplate")]
+        public async Task<BulkSendJobSendResponse> SendBulkWithEmbeddedTemplate(SignatureRequestBulkCreateEmbeddedWithTemplateRequest body)
+        {
+            return await new DBS().SendBulkWithEmbeddedTemplate(body);
+        }
+        //get the signature url of a contract
+        [HttpGet("GetEmbeddedSignature")]
+        public async Task<EmbeddedSignUrlResponse> EmbeddedSignUrl(string signature_id)
+        {
+            return await new DBS().EmbeddedSignUrl(signature_id);
+        }
+        //download files, as fileurl, as datauri
+        [HttpPost("DownloadFiles")]
+        public async Task<bool> DownloadFiles(string signatureRequestId, downloadType type)
+        {
+            return await new DBS().DownloadFiles(signatureRequestId, type);
+        }
+        //delete contract template
+        [HttpPost("DeleteContractTemplate")]
+        public async Task<bool> TemplateDelete(string templateId)
+        {
+            return await new DBS().TemplateDelete(templateId);
+        }
+
 
     }
 }
--- backend/DropBoxSign.cs
+++ backend/DropBoxSign.cs
@@ -2,6 +2,9 @@ using Dropbox.Sign.Api;
 using Dropbox.Sign.Client;
 using Dropbox.Sign.Model;
 using Newtonsoft.Json;
+using System.IO;
+using System.Reflection.Metadata;
+using static Dropbox.Api.Sharing.ListFileMembersIndividualResult;
 
 public class DropBoxSign
 {
@@ -16,13 +19,102 @@ public class DropBoxSign
 
     private protected EmbeddedApi embeddedApi = new EmbeddedApi(_config);
 
+    private protected TemplateApi templateApi = new TemplateApi(_config);
+
+    private protected OAuthApi oauthApi = new OAuthApi(_config);
+    private protected ApiAppApi apiAppApi = new ApiAppApi(_config);
+
+    public enum downloadType { files, datauri, fileurl }
+
+    //Authentication
+    public async Task<OAuthTokenResponse> OauthTokenGenerate(OAuthTokenGenerateRequest body)
+    {
+        var response = new OAuthTokenResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => oauthApi.OauthTokenGenerate(body));
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    public async Task<OAuthTokenResponse> OauthTokenRefresh(OAuthTokenRefreshRequest body)
+    {
+        var response = new OAuthTokenResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => oauthApi.OauthTokenRefresh(body));
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    //Accounts
+    public async Task<AccountCreateResponse> AccountCreate(AccountCreateRequest body)
+    {
+        var response = new AccountCreateResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => accountApi.AccountCreate(body));
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    public async Task<AccountGetResponse> AccountUpdate(AccountUpdateRequest body)
+    {
+        var response = new AccountGetResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => accountApi.AccountUpdate(body));
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
     public async Task<AccountGetResponse> AccountGet()
     {
         var account = new AccountGetResponse();
-    
+
         try
         {
-            account =  await Task.Run(() =>accountApi.AccountGet(null, "gtktorres@gmail.com"));
+            account = await Task.Run(() => accountApi.AccountGet(null, "gtktorres@gmail.com"));
         }
         catch (ApiException e)
         {
@@ -35,7 +127,109 @@ public class DropBoxSign
         return account;
     }
 
-    public async Task<SignatureRequestGetResponse> CreateSignature(SignatureRequestCreateEmbeddedRequest body)
+    public async Task<string> ApiAppDelete(string clientId)
+    {
+        string response = "";
+
+        try
+        {
+
+            await Task.Run(() => apiAppApi.ApiAppDelete(clientId));
+            Console.WriteLine("Account deleted.");
+            response = "Account deleted.";
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    //Contracts
+    public async Task<TemplateCreateEmbeddedDraftResponse> TemplateCreateEmbeddedDraft(TemplateCreateEmbeddedDraftRequest body)
+    {
+        var response = new TemplateCreateEmbeddedDraftResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => templateApi.TemplateCreateEmbeddedDraft(body));
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    public async Task<TemplateUpdateFilesResponse> TemplateUpdateFiles(string template_id, TemplateUpdateFilesRequest request)
+    {
+        var response = new TemplateUpdateFilesResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => templateApi.TemplateUpdateFiles(template_id, request));
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    public async Task<TemplateGetResponse> TemplateAddUser(string template_id, TemplateAddUserRequest request)
+    {
+        var response = new TemplateGetResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => templateApi.TemplateAddUser(template_id, request));
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+    public async Task<TemplateGetResponse> TemplateRemoveUser(string template_id, TemplateRemoveUserRequest request)
+    {
+        var response = new TemplateGetResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => templateApi.TemplateRemoveUser(template_id, request));
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    public async Task<SignatureRequestGetResponse> SignatureRequestCreateEmbedded(SignatureRequestCreateEmbeddedRequest body)
     {
         var response = new SignatureRequestGetResponse();
 
@@ -49,7 +243,7 @@ public class DropBoxSign
             Console.WriteLine("Status Code: " + e.ErrorCode);
             Console.WriteLine(e.StackTrace);
         }
-        catch(UriFormatException e)
+        catch (UriFormatException e)
         {
             Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
             Console.WriteLine("Status Code: " + e.HResult);
@@ -61,7 +255,49 @@ public class DropBoxSign
 
     }
 
-    public async Task<EmbeddedSignUrlResponse> GetEmbeddedSignURL(string signature_id)
+    public async Task<SignatureRequestGetResponse> SignatureRequestCreateEmbeddedWithTemplate(SignatureRequestCreateEmbeddedWithTemplateRequest body)
+    {
+        var response = new SignatureRequestGetResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => signatureRequestApi.SignatureRequestCreateEmbeddedWithTemplate(body));
+            Console.WriteLine("Bulk Signature requests sent.");
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    public async Task<BulkSendJobSendResponse> SendBulkWithEmbeddedTemplate(SignatureRequestBulkCreateEmbeddedWithTemplateRequest body)
+    {
+        var response = new BulkSendJobSendResponse();
+
+        try
+        {
+
+            response = await Task.Run(() => signatureRequestApi.SignatureRequestBulkCreateEmbeddedWithTemplate(body));
+            Console.WriteLine("Bulk Signature requests sent.");
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+        }
+
+        return response;
+    }
+
+    public async Task<EmbeddedSignUrlResponse> EmbeddedSignUrl(string signature_id)
     {
         var response = new EmbeddedSignUrlResponse();
 
@@ -77,5 +313,65 @@ public class DropBoxSign
         }
 
         return response;
+    }
+
+    public async Task<bool> DownloadFiles(string signatureRequestId, downloadType type)
+    {
+
+        try
+        {
+            switch (type)
+            {
+                case downloadType.files:
+
+                    using (FileStream fs = File.Create("file_response.pdf"))
+                    {
+                        var resultFiles = signatureRequestApi.SignatureRequestFiles(signatureRequestId, "pdf");
+                        await Task.Run(() => resultFiles.Seek(0, SeekOrigin.Begin));
+                        await Task.Run(() => resultFiles.CopyTo(fs));
+                        fs.Close();
+                        return true;
+                    }
+
+                case downloadType.fileurl:
+                    var resultFileURL = signatureRequestApi.SignatureRequestFilesAsFileUrl(signatureRequestId);
+                    return true;
+
+                case downloadType.datauri:
+                    var resultDataUri = signatureRequestApi.SignatureRequestFilesAsDataUri(signatureRequestId);
+                    return true;
+
+                default:
+                    Console.WriteLine("File download choice is invalid.");
+                    return false;
+            }
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+            return false;
+        }
+    }
+
+    public async Task<bool> TemplateDelete(string templateId) { 
+
+        try
+        {
+
+            await Task.Run(() => templateApi.TemplateDelete(templateId));
+            Console.WriteLine("Template deleted.");
+            return true;
+
+        }
+        catch (ApiException e)
+        {
+            Console.WriteLine("Exception when calling Dropbox Sign API: " + e.Message);
+            Console.WriteLine("Status Code: " + e.ErrorCode);
+            Console.WriteLine(e.StackTrace);
+            return false;
+        }
     }
 }

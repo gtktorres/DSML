26eb8ed89b0afffceec56708499a09679db50670
gtktorres <gtktorres@gmail.com>
2023-09-23 18:27:39 -0600

    Adds more Dropbox Sign API calls

--- backend/Controllers/DropBoxSignController.cs
+++ backend/Controllers/DropBoxSignController.cs
@@ -10,6 +10,28 @@ namespace backend.Controllers
     [Route("api/[controller]")]
     public class DropBoxSignController : ControllerBase
     {
+        //Authentication
+
+        [HttpPost("GenerateOAuthToken")]
+        public async Task<OAuthTokenResponse> GenerateOAuthToken([FromBody] OAuthTokenGenerateRequest body)
+        {
+            return await new DBS().OauthTokenGenerate(body);
+        }
+
+        [HttpPost("RefreshOAuthToken")]
+        public async Task<OAuthTokenResponse> RefreshOAuthToken([FromBody] OAuthTokenRefreshRequest body)
+        {
+            return await new DBS().RefreshOAuthToken(body);
+        }
+
+        //Accounts
+
+        [HttpPost("CreateAccount")]
+        public async Task<AccountCreateResponse> CreateAccount([FromBody] AccountCreateRequest body)
+        {
+            return await new DBS().CreateAccount(body);
+        }
+
         [HttpGet("GetAccount")]
         public async Task<AccountGetResponse> GetAccount()
         {
@@ -29,9 +51,9 @@ namespace backend.Controllers
         }
 
         //Contract
-        
+
         [HttpPost("CreateEmbeddedSignature")]
-        public async Task<SignatureRequestGetResponse> CreateSignature([FromBody]SignatureRequestCreateEmbeddedRequest body)
+        public async Task<SignatureRequestGetResponse> CreateSignature([FromBody] SignatureRequestCreateEmbeddedRequest body)
         {
             return await new DBS().CreateSignature(body);
         }
@@ -39,9 +61,8 @@ namespace backend.Controllers
         [HttpGet("GetEmbeddedSignature")]
         public async Task<EmbeddedSignUrlResponse> GetEmbeddedSignURL(string signature_id)
         {
-            return await new DBS().GetEmbeddedSignURL(signature_id);           
+            return await new DBS().GetEmbeddedSignURL(signature_id);
         }
-
         [HttpPost("DownloadFiles")]
         public async Task<bool> DownloadFiles(string signatureRequestId, downloadType type)
         {

73bd2e69801d0466942b02b51959608a13677b43
gtktorres <gtktorres@gmail.com>
2023-10-04 01:36:01 -0600

    includes an openai api that takes queries and return a response from chatGPT

--- chatGPT/Program.cs
+++ chatGPT/Program.cs
@@ -4,7 +4,10 @@ using chatGPT.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.Configure<OpenAIConfig>(builder.Configuration.GetSection("OpenAI"));
+builder.Services.AddOpenAi(settings =>
+{
+    settings.ApiKey = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("Rystem.OpenAi")["Key"];
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

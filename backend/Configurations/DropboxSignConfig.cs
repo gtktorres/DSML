ed03cc6faea5a864edf9d8d2c94be88413767395
gtktorres <gtktorres@gmail.com>
2024-09-23 11:05:57 -0600

    remove comments

--- backend/Configurations/DropboxSignConfig.cs
+++ backend/Configurations/DropboxSignConfig.cs
@@ -2,6 +2,6 @@
 {
     public class DropboxSignConfig
     {
-        public string Key { get; set; } = "APIKEY";
+        public string Key { get; set; } = "3d684057d1a90cf841bc16500d38507217014f92deffc2efaaa3135ea4a1cdb7";
     }
 }
